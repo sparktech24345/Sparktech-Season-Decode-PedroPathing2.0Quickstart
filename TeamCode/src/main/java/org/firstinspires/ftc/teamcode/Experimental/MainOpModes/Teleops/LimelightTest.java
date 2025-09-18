f85aabe7f1f344ce42445b266a6e4ed159bdcee3
GoForIt <csmenthusiast57@gmail.com>
2025-09-18 21:06:23 +0300

    succsesful limelight test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Experimental/MainOpModes/Teleops/LimelightTest.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.Experimental.MainOpModes.Teleops;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous
+public class LimelightTest extends OpMode {
+
+    private Limelight3A limelight3A;
+
+    @Override
+    public void init() {
+        limelight3A = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight3A.pipelineSwitch(0);
+        limelight3A.start();
+    }
+
+    @Override
+    public void loop() {
+        LLResult llResult = limelight3A.getLatestResult();
+        if (llResult != null && llResult.isValid()) {
+            telemetry.addData("X offset: ", llResult.getTx());
+            telemetry.addData("Y offset: ", llResult.getTy());
+            telemetry.addData("A offset: ", llResult.getTa());
+        }
+    }
+
+}
