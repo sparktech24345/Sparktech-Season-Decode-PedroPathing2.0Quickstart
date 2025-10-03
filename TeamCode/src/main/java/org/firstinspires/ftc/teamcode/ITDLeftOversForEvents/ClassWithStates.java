4693f13e4e0647254d782ed9d67565ad63afb4e2
DESKTOP-TBM70OS\Andrei <andrei.baloiu0@gmail.com>
2025-10-03 22:03:33 +0300

    Added ITD Teleop logic with all the needed helpers in order to participate at events

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ITDLeftOversForEvents/ClassWithStates.java
@@ -0,0 +1,302 @@
+package org.firstinspires.ftc.teamcode.ITDLeftOversForEvents;
+
+import static org.firstinspires.ftc.teamcode.ITDLeftOversForEvents.OrganizedPositionStorage.*;
+
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+
+public class ClassWithStates {
+
+    //state holders
+
+    //**************************************************************************\\
+
+    // intake
+    public static enum intakeStates{
+        noStateSet,
+        intakeExtended4out4,
+        intakeExtended3out4,
+        intakeExtended2out4,
+        intakeExtended1out4,
+        intakeRetracted,
+    }
+    public static intakeStates intakeState = intakeStates.noStateSet;
+
+    //*************************************************************************\\
+
+    //intake cabin
+
+    public static enum intakeCabinStates{
+        noStateSet,
+        intakeCabinDownCollecting,
+        intakeCabinDownOutputting,
+        intakeCabinDownStandStill,
+        intakeCabinTransferPosition,
+        intakeCabinFullInBot,
+        intakeCabinFullInBotOutputting, isInTransferToIntakeCabinDownCollecting,
+    }
+    public static intakeCabinStates intakeCabinState = intakeCabinStates.noStateSet;
+
+    //**************************************************************************\\
+
+    //outtake
+
+    public static enum outtakeStates{
+        noStateSet,
+        outtakeSpecimenHang,
+        outtakeBasket,
+        outtakeLowerBasket,
+        outtakeWallPickUpNormal,
+        outtakeWallPickUpNew,
+        outtakeTransfer,
+        outtakeStandBy,
+        outtakeStandByWithoutExtensions,
+        autoPark
+    }
+    public static outtakeStates outtakeState = outtakeStates.noStateSet;
+
+    //***************************************************************************\\
+
+    public static enum colorSensorOutty{
+        noSample,
+        correctSample,
+        wrongSample,
+    }
+
+    public static colorSensorOutty currentStateOfSampleInIntake = colorSensorOutty.noSample;
+
+    //***************************************************************************\\
+
+    public static enum colorList{
+        teamNotSet,
+        red,
+        blue,
+        yellow,
+    }
+
+    public static colorList currentTeam = colorList.teamNotSet;
+
+
+
+
+
+    //Intake States
+
+    public static void intakeExtended4out4(){
+        intakeState = intakeStates.intakeExtended4out4;
+        intakeExtendMotorTargetPos = 585;
+        intakeGravitySubtractor =8;
+    }
+    public static void intakeExtended3out4(){
+        intakeState = intakeStates.intakeExtended3out4;
+        intakeExtendMotorTargetPos = 377;
+        intakeGravitySubtractor =6;
+    }
+    public static void intakeExtended2out4(){
+        intakeState = intakeStates.intakeExtended2out4;
+        intakeExtendMotorTargetPos = 245;
+        intakeGravitySubtractor =4;
+    }
+    public static void intakeExtended1out4(){
+        intakeState = intakeStates.intakeExtended1out4;
+        intakeExtendMotorTargetPos = 112;
+        intakeGravitySubtractor =2;
+    }
+    public static void intakeRetracted(){
+        intakeState = intakeStates.intakeRetracted;
+        intakeExtendMotorTargetPos = 0;
+        intakeGravitySubtractor =0;
+    }
+
+
+
+
+
+
+    //intake cabin states
+
+    public static void intakeCabinDownCollecting(){
+        intakeCabinState = intakeCabinStates.intakeCabinDownCollecting;
+        intakePivotServoPos = intakePivotServoPickupPos;
+        isAfterTakingTakeySpiny = true;
+        intakeSpinMotorPow = -1;
+    }
+    public static void intakeCabinDownOutputting(){
+        intakeCabinState = intakeCabinStates.intakeCabinDownOutputting;
+        intakePivotServoPos = intakePivotServoPickupPos;
+        intakeSpinMotorPow = 0.6;
+    }
+    public static void intakeCabinALittleBitUpStandStill(){
+        intakeCabinState = intakeCabinStates.intakeCabinDownStandStill;
+        intakePivotServoPos = intakePivotServoPickupPos-5;
+        intakeSpinMotorPow = 0;
+    }
+    public static void intakeCabinALittleBitUpOutputting(){
+        intakeCabinState = intakeCabinStates.intakeCabinDownStandStill;
+        intakePivotServoPos = intakePivotServoPickupPos-5;
+        intakeSpinMotorPow = 0.9;
+    }
+    public static void intakeCabinTransferPosition(){
+        intakeCabinState = intakeCabinStates.intakeCabinTransferPosition;
+        intakePivotServoPos = intakePivotServoTransferPos;
+        intakeSpinMotorPow = 0;
+    }
+    public static void intakeCabinTransferPositionWithPower(){
+        intakeCabinState = intakeCabinStates.intakeCabinTransferPosition;
+        intakePivotServoPos = intakePivotServoTransferPos;
+        shouldStopIntakeCabinSpinningAfterTakig = true;
+        shouldStopIntakeCabinSpinningAfterTakigTimer = System.currentTimeMillis();
+        intakeSpinMotorPow = -0.5;
+    }
+    public static void intakeCabinFullInBot(){
+        intakeCabinState = intakeCabinStates.intakeCabinFullInBot;
+        intakePivotServoPos = intakePivotServoOutputTruBotPos;
+        intakeSpinMotorPow = 0;
+    }
+    public static void intakeCabinFullInBotOutputting(){
+        intakeCabinState = intakeCabinStates.intakeCabinFullInBotOutputting;
+        intakePivotServoPos = intakePivotServoOutputTruBotPos;
+        intakeSpinMotorPow = 0.9;
+    }
+
+
+
+
+
+
+    //Outtake States
+
+    public static void outtakePark(){
+        outtakeState = outtakeStates.autoPark;
+        outtakePivotServoPos = outtakePivotServoParkedPos;
+        outtakeClawServoPos = outtakeClawServoRetractedPos;
+        outtakeExtendMotorTargetPos = outtakeMotorParkedPos;
+    }
+
+    public static void outtakeSpecimenHang(){
+        outtakeState = outtakeStates.outtakeSpecimenHang;
+        outtakePivotServoPos = outtakePivotServoHighRungHangPos;
+        outtakeClawServoPos = outtakeClawServoRetractedPos;
+        outtakeExtendMotorTargetPos = outtakeSliderSpecimenHangPos;
+    }
+    /*public static void autoOuttakeSpecimenHang(){
+        outtakeState = outtakeStates.outtakeSpecimenHang;
+        outtakePivotServoPos = outtakePivotServoHighRungHangPos;
+        outtakeClawServoPos = outtakeClawServoRetractedPos;
+        outtakeExtendMotorTargetPos = autoOuttakeSliderSpecimenHangPos;
+    }//*/
+    public static void outtakeBasket(){
+        outtakeState = outtakeStates.outtakeBasket;
+        outtakeClawServoPos = outtakeClawServoRetractedPos;
+        outtakePivotServoPos = outtakePivotServoBasketPos;
+        if(!isInLowerBasketState) outtakeExtendMotorTargetPos = outtakeMotorMaxPos;
+        else outtakeExtendMotorTargetPos = outtakeMotorMaxPosLowerBasket;
+    }
+
+    public static void outtakeLowerBasket(){
+        outtakeState = outtakeStates.outtakeLowerBasket;
+        outtakeClawServoPos = outtakeClawServoRetractedPos;
+        outtakePivotServoPos = outtakePivotServoBasketPos;
+        outtakeExtendMotorTargetPos = outtakeMotorMaxPosLowerBasket;
+    }
+    /*public static void outtakeWallPickUpNormal(){ //old one
+        outtakeState = outtakeStates.outtakeWallPickUpNormal;
+        outtakePivotServoPos = 0;
+        outtakeClawServoPos = outtakeClawServoExtendedPos;
+        outtakeExtendMotorTargetPos = outtakeMotorActualZeroPos;
+    }//*/
+    public static void outtakeWallPickUpNew(){
+        outtakeState = outtakeStates.outtakeWallPickUpNew;
+        //outtakePivotServoPos = outtakePivotServoWallPickupPos;  //wire interference
+        isInNeedToGoToSpecimenTransferPos = true;
+        needsToExtraExtend = true;
+        outtakeClawServoPos = outtakeClawServoExtendedPos;
+        //outtakeIsInNeedToExtraExtendClaw = true;
+        outtakeIsInNeedToExtraExtendClawTimer = System.currentTimeMillis();
+        outtakeExtendMotorTargetPos = outtakeSlidersWallPickPos;
+    }
+    public static void autoOuttakeWallPickUpNew(){
+        outtakeState = outtakeStates.outtakeWallPickUpNew;
+        outtakePivotServoPos = outtakePivotServoWallPickupPos;  //wire interference
+        isInNeedToGoToSpecimenTransferPos = true;
+        needsToExtraExtend = true;
+        outtakeClawServoPos = outtakeClawServoExtendedPos;
+        //outtakeIsInNeedToExtraExtendClaw = true;
+        outtakeIsInNeedToExtraExtendClawTimer = System.currentTimeMillis();
+        outtakeExtendMotorTargetPos = outtakeSlidersWallPickPos;
+    }
+    /*public static void outtakeWallPickUpNewNoWireProtection(){
+        outtakeState = outtakeStates.outtakeWallPickUpNew;
+        outtakePivotServoPos = outtakePivotServoWallPickupPos; //wire interfereance
+        isInNeedToGoToSpecimenTransferPos = true;
+        outtakeClawServoPos = outtakeClawServoExtendedPos;
+        outtakeIsInNeedToExtraExtendClaw = true;
+        outtakeIsInNeedToExtraExtendClawTimer = System.currentTimeMillis();
+        outtakeExtendMotorTargetPos = outtakeSlidersWallPickPos;
+    }//*/
+    public static void outtakeTransfer(){
+        outtakeState = outtakeStates.outtakeTransfer;
+        outtakeClawServoPos = outtakeClawServoExtendedPos;
+        outtakePivotServoPos = outtakePivotServoTransferPos;
+        //outtakeExtendMotorTargetPos = outtakeMotorActualZeroPos;
+        isOuttakeInPositionToGoDown = true;
+        beforeOuttakeGoDownTimer = System.currentTimeMillis();
+    }
+    public static void autoOuttakeTransfer(){
+        outtakeState = outtakeStates.outtakeTransfer;
+        outtakeClawServoPos = outtakeClawServoExtendedPos;
+        outtakePivotServoPos = outtakePivotServoTransferPos;
+        outtakeExtendMotorTargetPos = outtakeMotorActualZeroPos;
+        isOuttakeInPositionToGoDown = true;
+        beforeOuttakeGoDownTimer = System.currentTimeMillis();
+    }
+    /*public static void outtakeStandByBasket(){
+        outtakeState = outtakeStates.outtakeStandBy;
+        outtakePivotServoPos = outtakePivotServoStandByPos;
+        outtakeExtendMotorTargetPos = outtakeMotorStandByPos;
+        //TO BE MEASURED
+    }//*/
+
+    public static void outtakeStandByWithoutExtensions(){
+        outtakeState = outtakeStates.outtakeStandByWithoutExtensions;
+        outtakePivotServoPos = outtakePivotServoStandByPos;
+        outtakeExtendMotorTargetPos = outtakeMotorActualZeroPos;
+        outtakeClawServoPos = outtakeClawServoRetractedPos;
+    }
+
+    public static colorSensorOutty ColorCompare(NormalizedRGBA colors, colorList currentTeam,boolean isYellowSampleNotGood){
+
+        if(!(colors.red > 0.006 || colors.blue > 0.004)) return colorSensorOutty.noSample;
+        colorList color=colorList.teamNotSet;
+
+        if (colors.red > colors.blue && colors.red > colors.green)
+            color = colorList.red;
+        if (colors.blue > colors.red && colors.blue > colors.green)
+            color = colorList.blue;
+        if (colors.green > colors.blue && colors.green > colors.red)
+            color = colorList.yellow;
+
+
+        //reversing team
+        colorList wrongSampleType = colorList.teamNotSet;
+        if(currentTeam == colorList.blue) wrongSampleType = colorList.red;
+        if(currentTeam == colorList.red) wrongSampleType = colorList.blue;
+
+        if(color == wrongSampleType) return colorSensorOutty.wrongSample;
+        if(isYellowSampleNotGood && color == colorList.yellow) return colorSensorOutty.wrongSample;
+        else return colorSensorOutty.correctSample;
+    }
+
+    //init method cuz why not
+    public static void initStates() {
+        outtakeStandByWithoutExtensions();
+        intakeCabinFullInBot();
+        intakeRetracted();
+    }
+
+    //wait method cuz why not
+
+    /*public void waitWhile(int timeToWait) {
+        long iniTime = System.currentTimeMillis();
+        while(iniTime + timeToWait < System.currentTimeMillis()){}
+    }*/
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ITDLeftOversForEvents/OrganizedPositionStorage.java
@@ -0,0 +1,267 @@
+package org.firstinspires.ftc.teamcode.ITDLeftOversForEvents;
+
+@com.acmerobotics.dashboard.config.Config
+public class OrganizedPositionStorage {
+    // MISC
+    public static double PIDincrement;
+    public static double gravityAdder;
+
+    // chassis / sasiu
+    public static double chassisBackLeftPow = 0;
+    public static double chassisBackRightPow = 0;
+    public static double chassisFrontLeftPow = 0;
+    public static double chassisFrontRightPow = 0;
+
+    // intake
+    public static double intakePivotServoPos;
+    public static double intakeSpinMotorPow;
+    public static double intakeExtendMotorTargetPos=0;
+    public static double intakeTargetPosAdder;
+    public static double intakeGravitySubtractor=0;
+
+    // outtake
+    public static double outtakePivotServoPos;
+    public static double outtakeClawServoPos;
+    public static double outtakeExtendMotorTargetPos=0;
+
+    public static double outtakeTargetPosAdder;
+
+
+
+
+
+
+    // CONSTANTE*
+
+
+    //Outtake
+
+    //outtake claw
+    public static double outtakeClawServoExtendedPos = 128;
+    public static double outtakeClawServoExtraExtendedPos = 208;
+    public static double outtakeClawServoRetractedPos = 35;
+
+
+    // outtake pivot
+    public static double outtakePivotServoWallPickupPos = 285;
+    public static double outtakePivotServoHighRungHangPos = 170;
+    public static double outtakePivotServoBasketPos = 43;
+    public static double outtakePivotServoTransferPos = 200;
+    public static double outtakePivotServoStandByPos = outtakePivotServoHighRungHangPos ;
+    public static double outtakePivotServoParkedPos = 0.53*328 - 9;
+
+
+    // outtake sliders
+
+    public static double outtakeMotorMaxPos = 2100;
+
+    public static double outtakeMotorMaxPosLowerBasket = 800;
+    public static double outtakeSliderSpecimenHangPos = 1100;
+    public static double autoOuttakeSliderSpecimenHangPos = 950;
+    public static double outtakeSlidersWallPickPos = 710;  //from 690
+    public static double outtakeMotorActualZeroPos = 0;
+    public static double outtakeMotorStandByPos = 1000;
+    public static double tempOuttakeAPosition = 0;
+    public static double outtakeMotorParkedPos = 655;
+
+
+
+    //intake
+
+    //intake pivot
+    public static double intakePivotServoPickupPos = 211;   // old 206
+    public static double intakePivotServoOutputTruBotPos = 15;
+    public static double intakePivotServoTransferPos = 153;
+    public static double tempIntakeTargetPastPosDifrence = 135;
+    public static double tempIntakeAPosition = 0;
+
+
+
+    // BOOLEANS
+
+    //misc
+    public static boolean isYellowSampleNotGood = false;
+    public static boolean reverseGamepad2 = false;
+    public static boolean isRobotInAuto = false;
+    public static boolean isInLowerBasketState = false;
+    public static boolean isColorSensorNotInUse = false;
+    public static boolean somethingFailed = false;
+    public static boolean shouldBeginZeroesRetune = false;
+    public static boolean isTimeToReset = false;
+    public static boolean shouldDoAutoSpecInTeleopBeggining = false;
+
+
+    //is pressed
+    public static boolean isPressedA1 = false;
+    public static boolean isPressedA2 = false;
+    public static boolean isPressedB1 = false;
+    public static boolean isPressedB2 = false;
+    public static boolean isPressedX1 = false;
+    public static boolean isPressedX1v2 = false;
+    public static boolean isPressedX2 = false;
+    public static boolean isPressedY1 = false;
+    public static boolean isPressedY2 = false;
+    public static boolean isPressedDL1 = false;
+    public static boolean isPressedD2Up = false;
+    public static boolean isPressedD2Down = false;
+
+
+    //intake stuff
+    public static boolean isAfterIntakeBeenDownColecting = false;
+    public static boolean isIntakeOutputting = false;
+    public static boolean isAfterBotHasBeenOutputting = false;
+    public static boolean isAfterOuttakeClawClosedAfterTransfer = false;
+    public static boolean isIntakeOutputtingManual = false;
+    public static boolean isIntakeSpinMOtorAfterJustTaking = false;
+    public static boolean isInCaseOfNotIntakeInBot = false;
+    public static boolean isInSpecimenState = false;
+    public static boolean isTimeToRefreshOutptingTime = false;
+    public static boolean isInPositionToRaiseOuttakeInOrderToEvadeIntake = false;
+    public static boolean shouldStopIntakeCabinSpinningAfterTakig = false;
+    public static boolean isAfterTakingTakeySpiny = false;
+    public static boolean wasIntakeCabinTruBotOutputting = false;
+    public static boolean isIntakeInPositionToOutputTruBot = false;
+    public static boolean hasIntakeOutputedTruBot = false;
+    public static boolean hasSmolOutputed = false;
+    public static boolean didTransfera = false;
+    public static int basketStandbyState = 0;
+
+    //outtake stuff
+    public static boolean isAfterOuttakeScoredSpecimen = false;
+    public static boolean isAfterOuttakeScoredBasketSample = false;
+    public static boolean isAfterOuttakeClosedClawAtWallSpecimen = false;
+    public static boolean isAtStateOfLettingBasketSampleGo = false;
+    public static boolean isInNeedToGoToSpecimenTransferPos = false;
+    public static boolean isOuttakeInPositionToGoDown = false;
+    public static boolean outtakeIsInNeedToExtraExtendClaw = false;
+    public static boolean isOuttakeInPositionToCloseClawForTransfer = false;
+    public static boolean isOuttakeInPositionToGoToStandBy = false;
+    public static boolean needsToExtraExtend = false;
+    public static boolean shouldAutoCollect = false;
+    public static boolean shouldTransfer = false;
+    public static boolean shouldSpecimenTransfer = false;
+    public static boolean isOuttakeAfterOutputedTruBot = false;
+    public static boolean justTransfered = false;
+
+
+
+
+
+    //LONGS / TIMERS
+
+    //auto stuff
+    public static long autoTimer;
+    public static long zeroesRetuneTimer;
+
+
+    //intake stuff
+
+    public static long intakeOutputtingTimer;
+    public static long intakeAfterTransferClosedClawTimer;
+    public static long intakeOutputtingTimerManual;
+    public static long intakeSpinMotorMorePowerAfterTakingTimer;
+    public static long isIntakeInBotTimer;
+
+    public static long timeSinceStartedMovingForTruBotOutput;
+    public static long waitingForOuttakeToEvadeIntakeTimer;
+    public static long shouldStopIntakeCabinSpinningAfterTakigTimer;
+    public static long hasSmolOutputedTimer;
+    public static long hasPressedXTimer;
+    public static long didTransferTime;
+
+
+
+    //outtake stuff
+    public static long outtakeSpecimenAfterScoreTimer;
+    public static long outtakeAfterBasketSampleScoreTimer;
+    public static long outtakeAfterHasClosedClawAtWallSpecimenTimer;
+    public static long beforeOuttakeGoDownTimer;
+    public static long outtakeIsInNeedToExtraExtendClawTimer;
+    public static long outtakeCloseClawInTransferTimer;
+    public static long outtakeGoToStandByTimer;
+    public static long isOuttakeAfterOutputedTruBotTimer;
+
+
+
+    public static void resetStuff() {
+        //toggles
+        Toggle.toggled = false;
+        Toggle.toggle_var = false;
+        Toggle.toggledButton2 = false;
+        Toggle.toggledVarButton2 = false;
+        isRobotInAuto = false;
+        isInLowerBasketState = false;
+        somethingFailed = false;
+        isColorSensorNotInUse = false;
+        shouldBeginZeroesRetune = false;
+        isTimeToReset = false;
+
+        // MISC
+        PIDincrement = 0;
+        gravityAdder = 0;
+
+        // intake
+        intakePivotServoPos = intakePivotServoTransferPos;
+        intakeExtendMotorTargetPos = 0;
+        intakeTargetPosAdder = 0;
+        intakeGravitySubtractor = 0;
+        tempIntakeAPosition = 0;
+
+        // outtake
+        outtakePivotServoPos = outtakePivotServoTransferPos;
+        outtakeClawServoPos = outtakeClawServoExtendedPos;
+        outtakeExtendMotorTargetPos = 0;
+        outtakeTargetPosAdder = 0;
+        tempOuttakeAPosition = 0;
+
+        // BOOLEANS
+        isYellowSampleNotGood = false;
+        reverseGamepad2 = false;
+
+        // is pressed
+        isPressedA1 = false;
+        isPressedA2 = false;
+        isPressedB1 = false;
+        isPressedB2 = false;
+        isPressedX1 = false;
+        isPressedX2 = false;
+        isPressedY1 = false;
+        isPressedY2 = false;
+        isPressedDL1 = false;
+        isPressedD2Up = false;
+        isPressedD2Down = false;
+
+        // intake stuff
+        isAfterIntakeBeenDownColecting = false;
+        isIntakeOutputting = false;
+        isAfterBotHasBeenOutputting = false;
+        isAfterOuttakeClawClosedAfterTransfer = false;
+        isIntakeOutputtingManual = false;
+        isInSpecimenState = false;
+        shouldStopIntakeCabinSpinningAfterTakig = false;
+        isAfterTakingTakeySpiny = false;
+        wasIntakeCabinTruBotOutputting = false;
+        isIntakeInPositionToOutputTruBot = false;
+        hasIntakeOutputedTruBot = false;
+        didTransfera = false;
+        hasSmolOutputed = false;
+
+        // outtake stuff
+        isAfterOuttakeScoredSpecimen = false;
+        isAfterOuttakeScoredBasketSample = false;
+        isAfterOuttakeClosedClawAtWallSpecimen = false;
+        isAtStateOfLettingBasketSampleGo = false;
+        outtakeIsInNeedToExtraExtendClaw = false;
+        isOuttakeInPositionToCloseClawForTransfer = false;
+        isOuttakeInPositionToGoToStandBy = false;
+        isOuttakeAfterOutputedTruBot = false;
+        justTransfered = false;
+
+        // LONGS / TIMERS
+        outtakeSpecimenAfterScoreTimer = 0;
+        outtakeAfterBasketSampleScoreTimer = 0;
+        outtakeAfterHasClosedClawAtWallSpecimenTimer = 0;
+        intakeOutputtingTimer = 0;
+        intakeAfterTransferClosedClawTimer = 0;
+    }
+}
