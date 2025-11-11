1b9abb9eb9d99ee311474df0f6a95f83fa3a5843
Umbin765 <tudorstefan.vaduva@gmail.com>
2025-11-11 20:04:03 +0200

    limelight fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Experimental/MainOpModes/Teleops/LimelightTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Experimental/MainOpModes/Teleops/LimelightTest.java
@@ -6,6 +6,7 @@ import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name = "Limelight Test", group = "Tests")
 public class LimelightTest extends OpMode {
@@ -13,6 +14,12 @@ public class LimelightTest extends OpMode {
     private Limelight3A limelight3A;
     private double old_pos_y_pruple = 0;
     private double ball_counter = 0;
+    private double old_Ty = 0;
+    private double old_Tx = 0;
+    private boolean doOnce = true;
+    ElapsedTime timp = new ElapsedTime();
+
+    private long timeLime = 0;
 
     @Override
     public void init() {
@@ -27,6 +34,8 @@ public class LimelightTest extends OpMode {
 
         telemetry.addLine("Limelight initialized and streaming...");
         telemetry.update();
+
+        timeLime = System.currentTimeMillis();
     }
 
     @Override
@@ -34,12 +43,24 @@ public class LimelightTest extends OpMode {
         LLResult llResult = limelight3A.getLatestResult();
 
         if(!limelight3A.isRunning()) limelight3A.start();
-
         if (llResult != null) {
             // Basic vision data
             telemetry.addData("X offset", llResult.getTx());
             telemetry.addData("Y offset", llResult.getTy());
             telemetry.addData("Target area", llResult.getTa());
+
+            if(doOnce){
+                old_Tx = llResult.getTx();
+                old_Ty = llResult.getTy();
+                doOnce = false;
+            }
+
+            if(old_Tx == llResult.getTx() && llResult.getTy() == old_Ty){
+                limelight3A.stop();
+                if (timp.milliseconds()<100) {
+                    limelight3A.start();
+                }
+            }
 
             // Optional: Python pipeline data
             double[] pythonOutputs = llResult.getPythonOutput();
@@ -49,8 +70,9 @@ public class LimelightTest extends OpMode {
                 double pos_y = pythonOutputs[1];
                 telemetry.addData("y_pos: ", pos_y);
                 telemetry.addData("y_old_pos: ", old_pos_y_pruple);
-                if(old_pos_y_pruple > 170 && pos_y - old_pos_y_pruple > 50){
+                if(old_pos_y_pruple > 190 && old_pos_y_pruple - pos_y > 50 && System.currentTimeMillis() - timeLime > 600){
                     ball_counter += 1;
+                    timeLime = System.currentTimeMillis();
                 }
                 old_pos_y_pruple = pos_y;
 
