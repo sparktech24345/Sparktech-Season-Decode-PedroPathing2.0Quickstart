78b0bb0ea18cc5c682b40b0722c64e4a5e556717
andreipreda2001 <andreipreda2001@yahoo.com>
2026-01-30 15:28:12 +0200

    added tested teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Experimental/MainOpModes/Teleops/LimelightTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Experimental/MainOpModes/Teleops/LimelightTest.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -16,174 +17,118 @@ import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.Experimental.HelperClasses.BallColorQueue;
 import org.firstinspires.ftc.teamcode.Experimental.HelperClasses.DecodeEnums.BallColorSet_Decode;
+import org.firstinspires.ftc.teamcode.Experimental.HelperClasses.RobotController;
+
+import java.util.List;
+
 @TeleOp(name = "Limelight Test", group = "Tests")
 public class LimelightTest extends OpMode {
 
 
     private Limelight3A limelight3A;
-    private double old_pos_y_purple = 0;
-    private double ball_counter = 0;
-    private double old_Ty = 0;
-    private double old_Tx = 0;
-    private boolean doOnce = true;
-    private double distance_to_Apriltag = 0;
-    private double ta = 0;
-    ElapsedTime timp = new ElapsedTime();
-
-    private long timeLime = 0;
+    public double camId = 0;
 
 
     /// ----------------- Color Sensor Stuff ------------------
-    private NormalizedColorSensor colorSensorGreen;
-    private NormalizedColorSensor colorSensorPurple;
-    private NormalizedColorSensor colorSensorLaunch;
-    private NormalizedRGBA greenSensorColors;
-    private NormalizedRGBA purpleSensorColors;
-    private NormalizedRGBA launchSensorColors;
+    protected NormalizedColorSensor colorSensorRight;
+    protected NormalizedColorSensor colorSensorLeft;
+    protected NormalizedRGBA rightSensorColors;
+    protected NormalizedRGBA leftSensorColors;
+    final float[] hsvRightSensorColors = new float[3];
+    final float[] hsvLeftSensorColors = new float[3];
+    public static int ballCounter = 0;
+    protected BallColorSet_Decode actualRightSensorDetectedBall;
+    protected BallColorSet_Decode calculatedRightSensorDetectedBall;
+    protected BallColorSet_Decode actualLeftSensorDetectedBall;
+    protected BallColorSet_Decode calculatedLeftSensorDetectedBall;
+    protected BallColorSet_Decode ballToFire;
+    public static boolean hasBallInRightChamber = false;
+    public static boolean hasBallInLeftChamber = false;
+    public static boolean shouldRemoveBalls = false;
+    BallColorQueue ballColorQueue = new BallColorQueue();
     private DcMotorEx intakeMotor;
-
-
-    final float[] hsvValuesGreen = new float[3];
-    final float[] hsvValuesPurple = new float[3];
-    final float[] hsvValuesLaunch = new float[3];
-    /// --------------------------------------------------------
 
     @Override
     public void init() {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
         limelight3A = hardwareMap.get(Limelight3A.class, "limelight");
-        limelight3A.pipelineSwitch(1);
+        limelight3A.pipelineSwitch(2);
         limelight3A.reloadPipeline();
         limelight3A.setPollRateHz(100); // poll 100 times per second
         limelight3A.start();
 
-        telemetry.addLine("Limelight initialized and streaming...");
-        telemetry.update();
+        intakeMotor = hardwareMap.get(DcMotorEx.class,intakeMotorName);
 
-//        colorSensorGreen = hardwareMap.get(NormalizedColorSensor.class, "greensensor");
-//        colorSensorPurple = hardwareMap.get(NormalizedColorSensor.class, "purplesensor");
-//        colorSensorLaunch = hardwareMap.get(NormalizedColorSensor.class, "launchsensor");
-
-        intakeMotor = hardwareMap.get(DcMotorEx.class,"intakemotor");
-
-
-        timeLime = System.currentTimeMillis();
+        colorSensorRight = hardwareMap.get(NormalizedColorSensor.class, colorSensorRightName);
+        colorSensorLeft = hardwareMap.get(NormalizedColorSensor.class, colorSensorLeftName);
     }
 
     @Override
     public void loop() {
-//        HandleColors();
-        LLResult llResult = limelight3A.getLatestResult();
-        telemetry.addData("ta", llResult.getTa());
-        telemetry.addData("distance", getDistanceToAprilTag(llResult.getTa()));
-
+        useCamera();
+        HandleColors();
+        shouldRemoveBalls = gamepad1.a;
         intakeMotor.setPower(gamepad1.left_stick_y);
-
-//        if(!limelight3A.isRunning()) limelight3A.start();
-//        if (llResult != null) {
-//            // Basic vision data
-//            telemetry.addData("X offset", llResult.getTx());
-//            telemetry.addData("Y offset", llResult.getTy());
-//            telemetry.addData("Target area", llResult.getTa());
-//
-//            if(doOnce){
-//                old_Tx = llResult.getTx();
-//                old_Ty = llResult.getTy();
-//                doOnce = false;
-//            }
-//
-//            if(old_Tx == llResult.getTx() && llResult.getTy() == old_Ty){
-//                limelight3A.stop();
-//                if (timp.milliseconds()>=100) {
-//                    limelight3A.start();
-//                }
-//            }
-//
-//            // Optional: Python pipeline data
-//            double[] pythonOutputs = llResult.getPythonOutput();
-//            if (pythonOutputs != null) {
-//                telemetry.addLine("Python Output:");
-//
-//                double pos_y = pythonOutputs[1];
-//                telemetry.addData("y_pos: ", pos_y);
-//                telemetry.addData("y_old_pos: ", old_pos_y_purple);
-//                if(old_pos_y_purple > 190 && old_pos_y_purple - pos_y > 50 && System.currentTimeMillis() - timeLime > 600){
-//                    ball_counter += 1;
-//                    timeLime = System.currentTimeMillis();
-//                }
-//                old_pos_y_purple = pos_y;
-//
-//                telemetry.addData("ball counter: ", ball_counter);
-//
-//                for (int i = 0; i < 8; i++) {
-//                    telemetry.addData("Value " + i, pythonOutputs[i]);
-//                }
-//            } else {
-//                telemetry.addLine("No Python output received.");
-//            }
-//        } else {
-//            telemetry.addLine("No Limelight data yet...");
-//        }
-        double tx = llResult.getTx();
-
-        telemetry.addData("tx", tx);
-
 
         telemetry.update();
     }
 
-    private void HandleColors() {
-
-
-        greenSensorColors =colorSensorGreen.getNormalizedColors();
-        purpleSensorColors =colorSensorPurple.getNormalizedColors();
-        launchSensorColors = colorSensorLaunch.getNormalizedColors();
-
-        Color.colorToHSV(greenSensorColors.toColor(), hsvValuesGreen);
-        Color.colorToHSV(purpleSensorColors.toColor(), hsvValuesPurple);
-        Color.colorToHSV(launchSensorColors.toColor(), hsvValuesLaunch);
-
-
-        greenSensorBall = BallColorSet_Decode.getColorForTurret(greenSensorColors);
-        purpleSensorBall1 = BallColorSet_Decode.getColorForTurret(purpleSensorColors);
-        launchSensorBall = BallColorSet_Decode.getColorForTurret(launchSensorColors);
-
-
-        telemetry.addData("G_RED",(double)greenSensorColors.red * 10000.0);
-        telemetry.addData("G_BLUE",(double)greenSensorColors.blue * 10000.0);
-        telemetry.addData("G_GREEN",(double)greenSensorColors.green * 10000.0);
-
-        telemetry.addData("P_RED",(double)purpleSensorColors.red * 10000.0);
-        telemetry.addData("P_BLUE",(double)purpleSensorColors.blue * 10000.0);
-        telemetry.addData("P_GREEN",(double)purpleSensorColors.green * 10000.0);
-
-        telemetry.addData("LAUNCH_RED",(double)launchSensorColors.red * 10000.0);
-        telemetry.addData("LAUNCH_BLUE",(double)launchSensorColors.blue * 10000.0);
-        telemetry.addData("LAUNCH_GREEN",(double)launchSensorColors.green * 10000.0);
-
-
-        greenSensorBall = BallColorSet_Decode.NoBall;
-        purpleSensorBall1 = BallColorSet_Decode.NoBall;
-        launchSensorBall = BallColorSet_Decode.NoBall;
-
-        telemetry.addData("GREEN_SENSOR_BALL", greenSensorBall);
-        telemetry.addData("PURPLE_SENSOR_BALL", purpleSensorBall1);
-        telemetry.addData("LAUNCH_SENSOR_BALL", launchSensorBall);
-    }
-
-    private void getDistance(){
+    public void useCamera() {
         LLResult llResult = limelight3A.getLatestResult();
-        ta = llResult.getTa();
-        telemetry.addData("ta", ta);
+        List<LLResultTypes.FiducialResult> fiducialResults = llResult.getFiducialResults();
+        for (LLResultTypes.FiducialResult fr : fiducialResults) {
+            camId = fr.getFiducialId();
+        }
+
+        telemetry.addData("april tag detected id",camId);
+        telemetry.addData("ty to tag",llResult.getTy());
     }
 
-    private double getDistanceToAprilTag(double targetArea) {
-        double a = 8.60403612;
-        double b = -0.0119936722;
+    protected void HandleColors() {
+        leftSensorColors = colorSensorLeft.getNormalizedColors();
+        rightSensorColors = colorSensorRight.getNormalizedColors();
 
-        return Math.log(targetArea / a) / b;
+        Color.colorToHSV(leftSensorColors.toColor(), hsvLeftSensorColors);
+        Color.colorToHSV(rightSensorColors.toColor(), hsvRightSensorColors);
+
+        actualLeftSensorDetectedBall = BallColorSet_Decode.getColorForStorage(leftSensorColors,true);
+        actualRightSensorDetectedBall = BallColorSet_Decode.getColorForStorage(rightSensorColors);
+
+
+        if (!shouldRemoveBalls) { // when not moving balls out of chambers they dont have permission to change to no ball
+            if (actualLeftSensorDetectedBall != BallColorSet_Decode.NoBall)
+                calculatedLeftSensorDetectedBall = actualLeftSensorDetectedBall;
+
+            if (actualRightSensorDetectedBall != BallColorSet_Decode.NoBall)
+                calculatedRightSensorDetectedBall = actualRightSensorDetectedBall;
+        }
+        else {
+            calculatedLeftSensorDetectedBall = actualLeftSensorDetectedBall;
+            calculatedRightSensorDetectedBall = actualRightSensorDetectedBall;
+        }
+
+        if (actualLeftSensorDetectedBall == null) actualLeftSensorDetectedBall = BallColorSet_Decode.NoBall;
+        if (actualRightSensorDetectedBall == null) actualRightSensorDetectedBall = BallColorSet_Decode.NoBall;
+
+        if (calculatedLeftSensorDetectedBall == null) calculatedLeftSensorDetectedBall = BallColorSet_Decode.NoBall;
+        if (calculatedRightSensorDetectedBall == null) calculatedRightSensorDetectedBall = BallColorSet_Decode.NoBall;
+
+        hasBallInLeftChamber = (calculatedLeftSensorDetectedBall != BallColorSet_Decode.NoBall);
+        hasBallInRightChamber = (calculatedRightSensorDetectedBall != BallColorSet_Decode.NoBall);
+
+        telemetry.addData("LEFT_RED", (double)leftSensorColors.red * 10000.0 * leftSensorColorMultiplier);
+        telemetry.addData("LEFT_BLUE", (double)leftSensorColors.blue * 10000.0 * leftSensorColorMultiplier);
+        telemetry.addData("LEFT_GREEN", (double)leftSensorColors.green * 10000.0 * leftSensorColorMultiplier);
+
+        telemetry.addData("RIGHT_RED", (double)rightSensorColors.red * 10000.0);
+        telemetry.addData("RIGHT_BLUE", (double)rightSensorColors.blue * 10000.0);
+        telemetry.addData("RIGHT_GREEN", (double)rightSensorColors.green * 10000.0);
+
+        telemetry.addData("LEFT Sensed Color", calculatedLeftSensorDetectedBall);
+        telemetry.addData("RIGHT Sensed Color", calculatedRightSensorDetectedBall);
     }
 
 }
