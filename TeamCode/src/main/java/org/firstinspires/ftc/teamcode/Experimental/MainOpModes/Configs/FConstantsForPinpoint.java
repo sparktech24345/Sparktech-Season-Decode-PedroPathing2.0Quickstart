f65fee50fb48a306a7692e2e2b42a77a4a9a67c4
Andrei Baloiu Acasa <andrei.baloiu0@gmail.com>
2026-02-05 23:01:29 +0200

    pids and motor name changed a bit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Experimental/MainOpModes/Configs/FConstantsForPinpoint.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Experimental/MainOpModes/Configs/FConstantsForPinpoint.java
@@ -1,4 +1,6 @@
 package org.firstinspires.ftc.teamcode.Experimental.MainOpModes.Configs;
+import static org.firstinspires.ftc.teamcode.Experimental.HelperClasses.GlobalStorage.*;
+
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.localization.Localizers;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -7,10 +9,10 @@ public class FConstantsForPinpoint { // TODO: de actualizat pt decode
     static {
         FollowerConstants.localizers = Localizers.PINPOINT;
 
-        FollowerConstants.leftFrontMotorName = "frontleft";
-        FollowerConstants.leftRearMotorName = "backleft";
-        FollowerConstants.rightFrontMotorName = "frontright";
-        FollowerConstants.rightRearMotorName = "backright";
+        FollowerConstants.leftFrontMotorName = frontLeftName;
+        FollowerConstants.leftRearMotorName = backLeftName;
+        FollowerConstants.rightFrontMotorName = frontRightName;
+        FollowerConstants.rightRearMotorName = backRightName;
 
         FollowerConstants.leftFrontMotorDirection = DcMotorSimple.Direction.REVERSE;
         FollowerConstants.leftRearMotorDirection = DcMotorSimple.Direction.REVERSE;
@@ -25,15 +27,15 @@ public class FConstantsForPinpoint { // TODO: de actualizat pt decode
         FollowerConstants.forwardZeroPowerAcceleration = -45;
         FollowerConstants.lateralZeroPowerAcceleration = -95;
 
-        FollowerConstants.translationalPIDFCoefficients.setCoefficients(0.1,0,0.01,0);
+        FollowerConstants.translationalPIDFCoefficients.setCoefficients(0.1,0,0.01,0.03);
         FollowerConstants.useSecondaryTranslationalPID = false;
         FollowerConstants.secondaryTranslationalPIDFCoefficients.setCoefficients(0.1,0,0.01,0); // Not being used, @see useSecondaryTranslationalPID
 
-        FollowerConstants.headingPIDFCoefficients.setCoefficients(1,0,0.1,0);
+        FollowerConstants.headingPIDFCoefficients.setCoefficients(1,0,0.1,0.3);
         FollowerConstants.useSecondaryHeadingPID = false;
         FollowerConstants.secondaryHeadingPIDFCoefficients.setCoefficients(2,0,0.1,0); // Not being used, @see useSecondaryHeadingPID
 
-        FollowerConstants.drivePIDFCoefficients.setCoefficients(0.006,0,0.0002,0.6,0);
+        FollowerConstants.drivePIDFCoefficients.setCoefficients(0.006,0,0.0002,0.6,0.03);
         FollowerConstants.useSecondaryDrivePID = false;
         FollowerConstants.secondaryDrivePIDFCoefficients.setCoefficients(0.1,0,0,0.6,0); // Not being used, @see useSecondaryDrivePID
 
